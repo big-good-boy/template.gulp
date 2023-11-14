5f4e45d3ab6b8094a67175f15af160c9024c7556
big-good-boy <drago999@bk.ru>
2023-11-14 22:06:44 +0500

    минификация js

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,5 +1,13 @@
+import minify from "gulp-minify";
+
 export const js = () => {
 	return app.gulp.src(app.path.dev.js)
+	.pipe(minify({
+		ext:{
+			src:'.js',
+			min:'.min.js'
+		}
+	}))
 	.pipe(app.gulp.dest(app.path.src.js))
 	.pipe(app.plugins.browserSync.stream())
 }
