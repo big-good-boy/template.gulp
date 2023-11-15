a3b5fb9ff35269e17575af12cd11f568aec5eb93
big-good-boy <drago999@bk.ru>
2023-11-15 17:35:36 +0500

    временная метка в подключениях css/ js

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,8 +1,10 @@
 import fileInclude from 'gulp-file-include';
+import replace from 'gulp-replace';
 
 export const html = () => {
 	return app.gulp.src(app.path.dev.html)
 		.pipe(fileInclude())
+		.pipe(replace('%NO_CACHE%', Date.now()))
 		.pipe(app.gulp.dest(app.path.src.html))
 		.pipe(app.plugins.browserSync.stream())
 }
