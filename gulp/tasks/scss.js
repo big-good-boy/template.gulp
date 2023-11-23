7fec4b00d63b791a753d43dcb6c9350303f1b2bf
big-good-boy <drago999@bk.ru>
2023-11-23 23:26:59 +0500

    добавил оформления для стартового шаблона

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -1,20 +1,24 @@
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 import autoprefixer from 'gulp-autoprefixer';
+import mediaQueries from "gulp-group-css-media-queries";
 
 const sass = gulpSass(dartSass);
 
 export const scss = () => {
 	return app.gulp.src(app.path.dev.scss)
+		.pipe(sass())
 		.pipe(autoprefixer({
 			grid: true,
 			overrideBrowserslist: ['last 5 versions'],
 			cascade: true
 		}))
-		.pipe(sass())                          // css без сжатия
-		.pipe(app.gulp.dest(app.path.src.css)) // css без сжатия
+		.pipe(mediaQueries())
+		.pipe(app.gulp.dest(app.path.src.css))
+
 		.pipe(sass({ outputStyle: 'compressed' }))
 		.pipe(app.plugins.concat('style.min.css'))
 		.pipe(app.gulp.dest(app.path.src.css))
+
 		.pipe(app.plugins.browserSync.stream())
 }
